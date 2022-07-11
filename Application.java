481c8f7a697a5ef615198a65568dbbb15f6b05ae
devsnemani <dev.snemani@gmail.com>
2022-07-10 23:09:23 -0400

    New file Application.Java ia addded

---
+++ Application.java
@@ -0,0 +1,7 @@
+working on the first project of devops
+git hun repo creation
+[A[C[C[C[C[C[C[b[D[D[[C[C[C[C[C[C[D[D[D[D[[3~[[C[C[[3~[3~[3~[C[C[C[C[C[C[C[C[C[C[C[C[hub repo creation, cloning
+[Dworking area - staging area - local area - [D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D(gitd file)[3~[C[[3~[3~[C[C[C[[3~[3~[3~[3~[3~[3~[3~[3~[C[C[C[C[C[C[C[C
+[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
+staging area( git commit -m) - local area
+remote repository (git push) local-remote
